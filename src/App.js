67187d8ea9d6d7f0c04739514ea304072f6de261
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-09-02 14:10:38 +0500

    Completed Tiny short Animation Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import Illustration from './components/illustration/Illustration';
 
 const App = () => {
     return (
-        <div>
-            Ahmed Faraz
-        </div>
+        <Fragment>
+            <nav>
+                <h1 className="logo">Tiny Animation</h1>
+            </nav>
+            <main>
+                <div className="content">
+                    <p>Tiny Short Animation</p>
+                    <h2>Developed using Web Animations API</h2>
+                    <p>Developed By</p>
+                    <h1>Ahmed Faraz</h1>
+                </div>
+                <div>
+                    <Illustration />
+                </div>
+            </main>
+        </Fragment>
     )
 }
 
